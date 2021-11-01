03a3c94cd3f69938dafae55904d6a413a77f9c21
Andrew Breckenridge <asbreckenridge@me.com>
2021-11-01 12:36:45 -0700

    init

---
+++ AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  fast-bar
+//
+//  Created by Andrew Breckenridge on 11/1/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
